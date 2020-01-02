2fdc3726066f2ec2a4af75826e581fb2614884c6
Anna Ryazantceva <anyariazantzeva@yandex.ru>
2020-01-02 12:39:03 +0200

    Delete item function

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 let itemsArray = [
-    {
-        id: 1,
-        value: 'Make Coffee',
-        important: false,
-        done: false
-    },
-    {
-        id: 2,
-        value: 'Wash Dishes',
-        important: false,
-        done: false
-    },
-    {
-        id: 3,
-        value: 'Learn Finnish',
-        important: false,
-        done: false
-    },
+    // {
+    //     id: 1,
+    //     value: 'Make Coffee',
+    //     important: false,
+    //     done: false
+    // },
+    // {
+    //     id: 2,
+    //     value: 'Wash Dishes',
+    //     important: false,
+    //     done: false
+    // },
+    // {
+    //     id: 3,
+    //     value: 'Learn Finnish',
+    //     important: false,
+    //     done: false
+    // },
 
 ];
 
@@ -35,7 +35,7 @@ const loadItems = (arr) => {
                    <button class="btn btn-edit">Edit</button>
               </div>
               <div class="app__delete">
-                   <button class="btn btn-delete">Delete</button>
+                   <button class="btn btn-delete" onclick="deleteItem()">Delete</button>
               </div>`;
         itemsList.appendChild(newElem);
     });
@@ -54,19 +54,43 @@ const addItem = () => {
         itemsArray.push(formElem);
         itemsList.innerHTML = '';
         loadItems(itemsArray);
+
         } else {
             alert('Enter a value');
         }
 
 };
 
-let addBtn = document.querySelector('.form__btn');
+const addBtn = document.querySelector('.form__btn');
 addBtn.addEventListener('click', (e) => {
     e.preventDefault();
     addItem();
+    console.log(itemsArray);
    document.querySelector('.form__control').value = '';
 });
 
-window.addEventListener('load', () => {
-   loadItems(itemsArray);
-});
+function deleteItem() {
+    let deleteButtons = document.querySelectorAll('.btn-delete');
+    deleteButtons.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            let target = e.target;
+            let parent = target.parentElement.parentElement.firstChild.firstChild;
+            let number = parent.innerHTML - 1;
+            itemsArray.splice(number, 1);
+            console.log(itemsArray);
+            itemsList.innerHTML = '';
+            loadItems(itemsArray);
+        });
+    });
+
+}
+
+// Count todos
+const countTodos = () => {
+
+}
+
+
+
+
